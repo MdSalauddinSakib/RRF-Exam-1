9ca79c8aa1801e577ff320153a27178b57e96697
Salauddin Sakib <salauddinsakib99@gmail.com>
2025-03-27 17:52:16 +0600

    header section create done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,20 +5,39 @@
 <template>
   <!--------------------
     Header Section
---------------------->
-  <section class=" bg-[#ffd4d0]">
-    <div class="ml-[120px] mr-[120px]">
+  --------------------->
+  <section class="bg-[#ffd4d0] relative font-montserrat px-[120px]">
+    <div class="w-36 h-36 bg-red-500 rounded-full absolute top-[-60px] left-[500px]"></div>
+    <div class="">
       <div class="flex justify-between items-center py-10">
-        <p class="text-[#FC3314] text-3xl font-semibold cursor-pointer"><span class="tracking-[-6px]">RR<span
-              class="text-black">F</span></span> EXAM</p>
-        <ul class="flex justify-between gap-8 font-bold text-lg">
+        <!-- Logo -->
+        <p class="text-3xl text-[#FC3314] font-semibold cursor-pointer"><span class="tracking-[-6px]"><span
+              class="text-black">RR</span>F</span>&nbsp;EXAM</p>
+        <!-- menu -->
+        <ul class="flex justify-between gap-8 font-bold text-lg z-10">
           <li class="hover:text-[#FC3314]"><a href="#">About</a></li>
           <li class="hover:text-[#FC3314]"><a href="#">Services</a></li>
           <li class="hover:text-[#FC3314]"><a href="#">Portfolio</a></li>
           <li class="hover:text-[#FC3314]"><a href="#">Blog</a></li>
         </ul>
-        <a href="#" class="bg-black text-white py-3 px-6 text-base font-bold hover:bg-[#4b413f]">Let's
-          Talk</a>
+        <a href="#" class="bg-black text-white py-3 px-6 text-base font-bold hover:bg-[#4b413f]">Let's Talk</a>
+      </div>
+    </div>
+    <!-- hero area -->
+    <div class="flex justify-between items-center text-[#1F1F1F]">
+      <div class="w-1/3 flex flex-col gap-6">
+        <p class="text-xl">UX Designer</p>
+        <h1 class="text-5xl font-bold leading-16">Hi There, I’m <br><span class="text-[#FC3314]">John Smith</span></h1>
+        <p>Welcome to my portfolio of captivating digital experiences. Explore my work and let's create something
+          extraordinary together.</p>
+
+        <div class="flex gap-2.5">
+          <a href="#" class="bg-black text-white py-3 px-14 text-base font-bold hover:bg-[#4b413f]">Hire Me</a>
+          <a href="#" class="py-3 px-14 border text-base font-bold hover:bg-[#4b413f] hover:text-white">Portfolio</a>
+        </div>
+      </div>
+      <div>
+        <img src="../assets/img/Profile & bg.png" alt="">
       </div>
     </div>
   </section>
