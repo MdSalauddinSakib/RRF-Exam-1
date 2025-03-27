c0f9e166a780a8e3b1f84565c1a8969dfbe2d05c
Salauddin Sakib <salauddinsakib99@gmail.com>
2025-03-27 16:01:01 +0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <!--------------------
+    Header Section
+--------------------->
+  <section class=" bg-[#ffd4d0]">
+    <div class="ml-[120px] mr-[120px]">
+      <div class="flex justify-between items-center py-10">
+        <p class="text-[#FC3314] text-3xl font-semibold"><span class="tracking-[-6px]">RR<span
+              class="text-black">F</span></span> EXAM</p>
+        <ul class="flex justify-between gap-8 font-bold text-lg">
+          <li class="hover:text-[#FC3314]"><a href="#">About</a></li>
+          <li class="hover:text-[#FC3314]"><a href="#">Services</a></li>
+          <li class="hover:text-[#FC3314]"><a href="#">Portfolio</a></li>
+          <li class="hover:text-[#FC3314]"><a href="#">Blog</a></li>
+        </ul>
+        <a href="#" class="bg-black text-white py-3 px-6">Let's Talk</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
